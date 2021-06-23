ac00bcf883d40662b240b557b074321482e7a28d
Ronnen Nagal <nagalr@gmail.com>
2021-06-23 16:33:20 +0300

    Initial Code commit.

--- src/main/java/com/bafoly/pagination/user/UserController.java
+++ src/main/java/com/bafoly/pagination/user/UserController.java
@@ -1,5 +1,20 @@
 package com.bafoly.pagination.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
 public class UserController {
-  
+
+    private final UserService userService;
+
+    @GetMapping("/api/1.0/users")
+    List<User> getUsers() {
+        return userService.getUsers();
+    }
+
 }
--- src/main/java/com/bafoly/pagination/user/UserService.java
+++ src/main/java/com/bafoly/pagination/user/UserService.java
@@ -1,5 +1,17 @@
 package com.bafoly.pagination.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
 public class UserService {
-  
+
+    private final UserRepository userRepository;
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
 }
