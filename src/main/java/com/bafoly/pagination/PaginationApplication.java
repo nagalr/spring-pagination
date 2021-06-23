ec2b7a8a1a98f8b037f2964ab5b3b0d91d1f8d25
Ronnen Nagal <nagalr@gmail.com>
2021-06-23 17:03:27 +0300

    Convert the runner method to lambda.

--- src/main/java/com/bafoly/pagination/PaginationApplication.java
+++ src/main/java/com/bafoly/pagination/PaginationApplication.java
@@ -16,6 +16,7 @@ public class PaginationApplication {
 		SpringApplication.run(PaginationApplication.class, args);
 	}
 
+	// populate 30 Users in H2 DB
 	@Bean
 	CommandLineRunner run(UserRepository userRepository) {
 		return args -> IntStream.rangeClosed(1, 30).forEach(i -> {
@@ -27,5 +28,4 @@ public class PaginationApplication {
 			userRepository.save(user);
 		});
 	}
-
 }
