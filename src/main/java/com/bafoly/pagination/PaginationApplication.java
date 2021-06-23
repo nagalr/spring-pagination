e7743ee3d6b45d08b64d65becd85207481a65a08
Ronnen Nagal <nagalr@gmail.com>
2021-06-23 16:55:47 +0300

    Implement a CommandLneRunner in main.

--- src/main/java/com/bafoly/pagination/PaginationApplication.java
+++ src/main/java/com/bafoly/pagination/PaginationApplication.java
@@ -1,13 +1,31 @@
 package com.bafoly.pagination;
 
+import com.bafoly.pagination.user.User;
+import com.bafoly.pagination.user.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class PaginationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaginationApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(UserRepository userRepository) {
+		return args -> IntStream.rangeClosed(1, 30).forEach(i -> {
+			User user = User.builder()
+					.username("user" + i)
+					.password("password")
+					.email("user" + i + "@gmail.com")
+					.build();
+			userRepository.save(user);
+		});
 	}
 
 }
--- src/main/java/com/bafoly/pagination/user/User.java
+++ src/main/java/com/bafoly/pagination/user/User.java
@@ -1,13 +1,19 @@
 package com.bafoly.pagination.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.Data;
-
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 public class User {
 
